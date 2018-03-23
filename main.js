1e18e557f25e25ffe8c228da156e8df0fb656c19
glidev5 <glidev5@gmail.com>
2018-03-23 13:04:08 -0400

    moved loggin to use util.debug to speed up production

--- main.js
+++ main.js
@@ -4,13 +4,14 @@
 var exec = require('child_process').exec;
 var doq=require('gqdoq');
 var dosave=""; // change this to "--save" to have modules added to package.json
+var debug=require("util").debug;
 
 // this installs given local module
 function installModule(o, cb) {
-    console.log("trying to install module: "+ o.module);
+    debug("trying to install module: "+ o.module);
     exec('npm install '+dosave +" "+ o.module, function callback(error, stdout, stderr) {
         if(error){
-            console.log(error)
+            debug(error)
         }
         cb(error,o);
     });
@@ -27,10 +28,10 @@ exports.installModule=q_installModule;
 // this installs given local module
 function uninstallModule(o, cb) {
     o.module= o.module.split('@')[0];
-    console.log("trying to uninstall module: "+ o.module);
+    debug("trying to uninstall module: "+ o.module);
     exec('npm uninstall ' + o.module, function callback(error, stdout, stderr) {
         if (error) {
-            console.log(error.stack);
+            debug(error.stack);
         }
         cb(error,o);
     });
@@ -49,7 +50,7 @@ function npmInstall(o,cb){
     o=o||{};
     exec('npm install', function callback(error, stdout, stderr) {
         if (error) {
-            console.log(error.stack);
+            debug(error.stack);
         }
         cb(error,o);
     });
@@ -67,7 +68,7 @@ exports.npmInstall=q_npmInstall;
 function newRequire(o, cb) {
     cb=cb||function(e,r){
         if(e)
-            console.log(e.stack);
+            debug(e.stack);
     };
 
     if(!o.module){
@@ -78,18 +79,18 @@ function newRequire(o, cb) {
 
     try {
         o.result = require(o.moduleName);
-        console.log("normal required: "+ o.module);
+        debug("normal required: "+ o.module);
         cb(null, o);
         return o.result;
     } catch (e) {
         installModule(o, function (err, resp) {
             try {
                 o.result = require(o.moduleName);
-                console.log("success install required: "+ o.module);
+                debug("success install required: "+ o.module);
                 cb(null, o);
                 return o.result;
             } catch (e) {
-                console.log("failed to install require: "+ o.module);
+                debug("failed to install require: "+ o.module);
                 //console.log(e.stack);
                 cb(e, o);
                 return null;
